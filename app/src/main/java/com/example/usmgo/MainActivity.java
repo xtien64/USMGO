8433752d1d865debe149bd73f02d15e77cc03a26
Chieng Xing Tien <xingtien@student.usm.my>
2022-12-28 23:19:03 +0800

    Add Permission and Passenger UI

--- app/src/main/java/com/example/usmgo/MainActivity.java
+++ app/src/main/java/com/example/usmgo/MainActivity.java
@@ -2,10 +2,23 @@ package com.example.usmgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -14,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkMyPermission();
 
         button = (Button) findViewById(R.id.button_signin);
         button.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +42,31 @@ public class MainActivity extends AppCompatActivity {
     public void openSignIn() {
         Intent intent = new Intent(this, sign_in.class);
         startActivity(intent);
+    }
+
+    private void checkMyPermission(){
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Toast.makeText(MainActivity.this,"Permission Granted",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(),"");
+                        intent.setData(uri);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
     }
 }
--- app/src/main/java/com/example/usmgo/passenger.java
+++ app/src/main/java/com/example/usmgo/passenger.java
@@ -1,12 +1,47 @@
 package com.example.usmgo;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.usmgo.databinding.PassengerBinding;
 
 public class passenger extends AppCompatActivity {
+
+    PassengerBinding binding;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.passenger);
+        //setContentView(R.layout.passenger);
+        binding = PassengerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.Home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.History:
+                    replaceFragment(new HistoryFragment());
+                    break;
+                case R.id.Profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_passenger,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
