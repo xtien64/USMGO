dd214b8b98b64cb7ecfcc77d0b2b43e71264411f
Catherine Lam Pui Kiew <puikiew14@gmail.com>
2023-01-02 11:48:33 +0800

    Admin Layout

--- app/src/main/java/com/example/usmgo/MainActivity.java
+++ app/src/main/java/com/example/usmgo/MainActivity.java
@@ -47,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //test payment part
+        button = (Button) findViewById(R.id.button_admin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), admin.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void openSignIn() {
